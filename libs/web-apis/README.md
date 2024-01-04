eafeb417328c144425da5bf058749d57b3a71131
Eduard <eduard.aminov.160@gmail.com>
2024-01-04 16:59:10 +0300

    feat(lerna): install

---
+++ libs/web-apis/README.md
@@ -0,0 +1,7 @@
+# web-apis
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test web-apis` to execute the unit tests.
---
+++ libs/web-apis/src/lib/navigator/tokens/navigator.ts
@@ -0,0 +1,5 @@
+import { InjectionToken } from '@angular/core';
+
+export const NAVIGATOR = new InjectionToken('[NAVIGATOR]', {
+    factory: () => window.navigator,
+});
